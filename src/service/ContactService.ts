05c943abd654ad493ab8e659c59e27d3bda23cbb
Zuhaib Ahmed <zuhaibahmed444@gmail.com>
2024-05-29 18:05:07 +0530

    pass all the tests

--- src/service/ContactService.ts
+++ src/service/ContactService.ts
@@ -23,8 +23,8 @@ export class ContactService {
       ]});
     }
 
-    console.log(contacts);
     if (contacts.length === 0) {
+      //  to add the primary contact
       primaryContact = this.entityManager.create(Contact, {
         phoneNumber,
         email,
@@ -32,6 +32,7 @@ export class ContactService {
       } as Contact);
       await this.entityManager.persistAndFlush(primaryContact);
     } else if (contacts.length >1){
+      // if already primary and secondary exist
       contacts.forEach((contact) =>{
         if(contact.linkedId){
           secondaryContacts.push(contact);
@@ -39,19 +40,32 @@ export class ContactService {
           primaryContact = contact
         }
       })
+      if(this.allPrimaryContacts(contacts)){
+        // if only primary exist and convert to primary 
+        contacts.sort((a,b) => a.id - b.id);
+        primaryContact = contacts[0];
+        const contactToUpdate = this.entityManager.create(Contact, { id : contacts[1].id, linkedId : contacts[0].linkedId, email : contacts[1].email ,phoneNumber : contacts[1].phoneNumber, createdAt : contacts[1].createdAt, updatedAt : new Date() , deletedAt: contacts[1].deletedAt,linkPrecedence :'secondary'})
+        await this.entityManager.upsert(contactToUpdate);
+        secondaryContacts.push(contactToUpdate);
+        return this.createResponse.create(primaryContact!, secondaryContacts)
+      }
     }else if (contacts.length > 0 && contacts[0].linkedId){
-      console.log("I am here in FIRST1",contacts)
+      // if secondary contacts email is passed 
       primaryContact = await this.entityManager.findOne(Contact, { id : contacts[0].linkedId})
       secondaryContacts = contacts;
     }else if (contacts.length > 0 && !contacts[0].linkedId){
-      console.log("I am in the second",contacts)
+      // if primary contacts email is passed
       primaryContact = contacts[0];
+
+      // find secondary based on primary
       const secondaryContactsFromprimary = await this.entityManager.find(Contact, {  $or: [
         { email : primaryContact.email},
         { phoneNumber: primaryContact.phoneNumber }
       ], linkPrecedence:'secondary'});
+
+
       if(secondaryContactsFromprimary.length === 0){
-        console.log("I am in the THIRD",contacts)
+        // create secondary if does not exist
         const secondaryContact = this.entityManager.create(Contact, {
           phoneNumber,
           email,
@@ -61,10 +75,21 @@ export class ContactService {
         secondaryContacts.push(secondaryContact);
         await this.entityManager.persistAndFlush(secondaryContact);
       }else{
-        console.log("I am in the FOURTH",contacts)
+        // if secondary already exists
         secondaryContacts = secondaryContactsFromprimary
       }
     }
+
     return this.createResponse.create(primaryContact!, secondaryContacts);
   }
+
+
+  private allPrimaryContacts(contacts : Contact[]): boolean{
+    console.log("Inthe primary contacts validatios")
+    contacts.forEach((contact) =>{
+      if(contact.linkPrecedence !== 'primary') return false;
+    })
+    return true
+  }
+  
 }
